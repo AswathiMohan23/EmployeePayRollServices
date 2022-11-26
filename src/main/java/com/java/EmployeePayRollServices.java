9094ef9ddad3e75c6295ae06ebdb540c474cbfff
AswathiMohan23 <achu999here@gmail.com>
2022-11-26 23:08:04 +0530

    [ADDED] : code to create employee payroll services

--- src/main/java/com/java/EmployeePayRollServices.java
+++ src/main/java/com/java/EmployeePayRollServices.java
@@ -1,7 +1,37 @@
 package com.java;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeePayRollServices {
+    private List< EmployeePayrollData>employeePayrollList;
+
+    public EmployeePayRollServices(ArrayList<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList=employeePayrollList;
+    }
+
     public static void main(String[] args) {
-        System.out.println("WELCOME TO EMPLOYEE PAY ROLL SERVICES");
+        System.out.println("Welcome to Employee Pay roll Services");
+        ArrayList<EmployeePayrollData>employeePayrollList=new ArrayList<EmployeePayrollData>();
+        EmployeePayRollServices employeePayRollServices =new EmployeePayRollServices(employeePayrollList);
+        Scanner sc=new Scanner(System.in);
+        employeePayRollServices.readEmployeePayrollData(sc);
+        employeePayRollServices.writeEmployeePayrollData();
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println("\nWriting employee payRoll data o console\n"+employeePayrollList);
+    }
+
+    private void readEmployeePayrollData(Scanner sc) {
+        System.out.println("Enter employee Name : ");
+        String name=sc.next();
+        System.out.println("Enter employee id : ");
+        int id =sc.nextInt();
+        System.out.println("Enter employee Salary : ");
+        double salary=sc.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(name,id,salary));
     }
 }
+
