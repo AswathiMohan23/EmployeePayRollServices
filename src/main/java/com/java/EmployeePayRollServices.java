85730f280ce4fc614af7acdc3e9843cc3a02b48e
AswathiMohan23 <achu999here@gmail.com>
2022-11-26 23:05:28 +0530

    [ADDED] : welcome message

---
+++ src/main/java/com/java/EmployeePayRollServices.java
@@ -0,0 +1,7 @@
+package com.java;
+
+public class EmployeePayRollServices {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO EMPLOYEE PAY ROLL SERVICES");
+    }
+}
