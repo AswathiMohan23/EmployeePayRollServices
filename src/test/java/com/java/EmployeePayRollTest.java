dd875f9e6f07fb0b8c5b98db0fe338c8196d9985
AswathiMohan23 <achu999here@gmail.com>
2022-11-27 13:02:25 +0530

    [ADDED] :code for watch service

--- src/test/java/com/java/EmployeePayRollTest.java
+++ src/test/java/com/java/EmployeePayRollTest.java
@@ -12,19 +12,19 @@ import java.nio.file.Paths;
 import java.util.stream.IntStream;
 
 public class EmployeePayRollTest {
-    private static String HOME =System.getProperty("user.home"); // cd ~ gives home directory
+    private static String HOME = System.getProperty("user.home"); // cd ~ gives home directory
     private static String PLAYGROUND = "PlayGround";
 
     @Test
-    public void givenPathWhenCheckedThenConfirm() throws IOException{
+    public void givenPathWhenCheckedThenConfirm() throws IOException {
 
         //checking whether file exists
-        Path homePath= Paths.get(HOME);
+        Path homePath = Paths.get(HOME);
         Assert.assertTrue(Files.exists((homePath)));
 
         //delete file and check file not exist
-        Path playPath=Paths.get(HOME+"/"+PLAYGROUND);
-        if(Files.exists(playPath))FileUtils.deleteFiles(playPath.toFile());
+        Path playPath = Paths.get(HOME + "/" + PLAYGROUND);
+        if (Files.exists(playPath)) FileUtils.deleteFiles(playPath.toFile());
         Assert.assertTrue(Files.notExists(playPath));
 
         //create directory
@@ -33,17 +33,27 @@ public class EmployeePayRollTest {
 
         //create file
 
-        IntStream.range(1,10).forEach(cntr->{
-            Path tempFile=Paths.get(playPath+"/temp"+cntr);
+        IntStream.range(1, 10).forEach(cntr -> {
+            Path tempFile = Paths.get(playPath + "/temp" + cntr);
             Assert.assertTrue(Files.notExists(tempFile));
-            try{
-                Files.createFile(tempFile); }
-            catch (IOException e){
+            try {
+                Files.createFile(tempFile);
+            } catch (IOException e) {
                 Assert.assertTrue(Files.exists(tempFile));
             }
         });
 
+        //List files,directories as well as files with extension
+        Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+        Files.newDirectoryStream(playPath).forEach(System.out::println);
+        Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
+                .forEach(System.out::println);
     }
 
-
+    @Test
+    public void giveADirectoryWhenWatchedListsAllTheActivities() throws IOException{
+        Path dir=Paths.get(HOME+"/"+PLAYGROUND);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new WatchServicePayRoll(dir).processEvents();
+    }
 }
